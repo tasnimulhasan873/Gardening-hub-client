bb20271a5ad928edf61d3392115e92d944c85d32
tasnimulhasan873 <tasnimul873@gmail.com>
2025-05-19 21:00:39 +0600

    routing done

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,65 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router";
 
-createRoot(document.getElementById('root')).render(
+import App from "./App.jsx";
+
+import { HelmetProvider } from "react-helmet-async";
+import Login from "./pages/Login.jsx";
+import Register from "./pages/Register.jsx";
+import Footer from "./components/Footer.jsx";
+import NotFound from "./pages/NotFound.jsx";
+import ShareTip from "./pages/ShareTip.jsx";
+import TipDetails from "./pages/TipDetails.jsx";
+import UpdateTip from "./pages/UpdateTip.jsx";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "/login", element: <Login /> },
+      { path: "/register", element: <Register /> },
+      { path: "/footer", element: <Footer /> },
+      {
+        path: "/shareTip",
+        element: (
+          <PrivateRoute>
+            <ShareTip />
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: "tipDetails",
+        element: (
+          <PrivateRoute>
+            <TipDetails />
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: "/updateTip",
+        element: (
+          <PrivateRoute>
+            <UpdateTip />
+          </PrivateRoute>
+        ),
+      },
+
+      { path: "*", element: <NotFound /> },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <AuthProvider>
+      <HelmetProvider>
+        <RouterProvider router={router} />
+      </HelmetProvider>
+    </AuthProvider>
+  </StrictMode>
+);
